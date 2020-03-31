bfa61d34eedb3bcf994a9c95df957f43b7c912de
Oliver Brown <akuma_shadow@hotmail.com>
2020-03-31 09:55:59 -0400

    actualizar las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { ComponentsComponent } from './components/components.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InterpolationComponent } from './interpolation/interpolation.component';
 
-
-const routes: Routes = [];
+// Constante en donde se declaran las rutas para luego ser utilizadas
+const routes: Routes = [
+  { path: 'hello-world', component: HelloWorldComponent },
+  { path: 'components', component: ComponentsComponent},
+  { path: 'interpolation', component: InterpolationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { ComponentsComponent } from './components/components.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { Componente1Component } from './components/componente1/componente1.component';
+import { Componente2Component } from './components/componente2/componente2.component';
+import { Componente3Component } from './components/componente3/componente3.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWorldComponent
+    HelloWorldComponent,
+    ComponentsComponent,
+    ToolBarComponent,
+    Componente1Component,
+    Componente2Component,
+    Componente3Component,
+    InterpolationComponent
   ],
   imports: [
     BrowserModule,
